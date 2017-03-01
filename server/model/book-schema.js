c888a2c5da9d73b8284b63b793e38d4c9340f2d1
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-01 14:16:02 +0100

    Added book-schema and test-utils for unit testing.

---
+++ server/model/book-schema.js
@@ -0,0 +1,75 @@
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+/* BookSchema is used for storing info about all the books in the club. As there
+ * can be multiple copies of same title, each "physical" book has a unique ID
+ * and owner of course. */
+const BookSchema = Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    year: {
+        type: Number
+    },
+    author: {
+        type: String
+    },
+    thumbnail: {
+        type: String
+    },
+    owner: {
+        type: ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tradeReqs: [{type: ObjectId, ref: 'TradeReq'}]
+
+},
+{collection: 'books'}
+);
+
+//---------------------------------------------------------------------------
+// STATIC METHODS
+//---------------------------------------------------------------------------
+
+BookSchema.statics.create = function(obj, cb) {
+    var Book = this.model('Book');
+    var newBook = new Book();
+    newBook.title = obj.title;
+    newBook.year = obj.year;
+    newBook.owner = obj.owner;
+    newBook.author = obj.author;
+    newBook.thumbnail = obj.thumbnail;
+
+    newBook.save( (err, data) => {
+        if (err) {cb(err);}
+        else {cb(null, data);}
+    });
+
+};
+
+//---------------------------------------------------------------------------
+// INSTANCE METHODS
+//---------------------------------------------------------------------------
+
+/* Updates the book info with given object.*/
+BookSchema.methods.update = function(obj, cb) {
+    var setVals = {$set: obj};
+    this.model('Book').update({_id: this._id}, setVals, {}, (err) => {
+        if (err) {cb(err);}
+        cb(null);
+    });
+};
+
+BookSchema.methods.changeOwner = function(userID, cb) {
+    var obj = {owner: userID};
+    this.update(obj, cb);
+};
+
+module.exports = mongoose.model('Book', BookSchema);
---
+++ test/test-utils.js
@@ -0,0 +1,32 @@
+
+
+const mongoose = require('mongoose');
+const expect = require('chai').expect;
+
+var TestUtils = {
+
+    getObjectId: function() {
+        return mongoose.Types.ObjectId();
+    },
+
+    expectEqualObjectId: function(got, exp) {
+        var msg = 'Object IDs match';
+        expect(got.toString(), msg).to.be.equal(exp.toString());
+    },
+
+    connectTestDb: function() {
+        var testDbURI = 'mongodb://localhost:27017/bookwyrmstest';
+        mongoose.Promise = global.Promise;
+        mongoose.connect(testDbURI);
+    }
+};
+
+TestUtils.createTradeReq = function() {
+    return {
+        from: TestUtils.getObjectId(),
+        to: TestUtils.getObjectId(),
+        book: TestUtils.getObjectId()
+    };
+};
+
+module.exports = TestUtils;
