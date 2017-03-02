ef72bfed5ad8842b1f2ba586e821917b45ba836d
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-02 22:21:22 +0100

    Added constructor and onClick method.

--- client/jsx/book-item.jsx
+++ client/jsx/book-item.jsx
@@ -5,12 +5,24 @@ const React = require('react');
 
 class BookItem extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+        this.props.onClick(this.props.book);
+    }
+
     render() {
         var book = this.props.book;
         return (
             <div className='book-item'>
                 <ul>
-                    <li>Title: {book.title}</li>
+                    <li>
+                        Title: {book.title}
+                        <button onClick={this.onClick}>X</button>
+                    </li>
                 </ul>
             </div>
         );
@@ -18,7 +30,8 @@ class BookItem extends React.Component {
 }
 
 BookItem.propTypes = {
-    book: React.PropTypes.object
+    book: React.PropTypes.object,
+    onClick: React.PropTypes.func
 };
 
 module.exports = BookItem;
