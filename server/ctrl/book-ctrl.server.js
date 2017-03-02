47065f8edf3464462360802c0bc85915f191fec6
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-02 22:22:57 +0100

    Finalized addBook and started to implement deleteBook.

--- server/ctrl/book-ctrl.server.js
+++ server/ctrl/book-ctrl.server.js
@@ -1,20 +1,45 @@
 'use strict';
 
 const Book = require('../model/book-schema');
+const User = require('../model/user-schema');
 
 /* Server-side book controller which manipulates the books stored in the
  * database.
  */
 class BookController {
 
-    /* Adds one book to the database.*/
+    /* Adds one book to the database and for the user.*/
     addBook(book, cb) {
-        Book.create(book, (err, data) => {
+        User.getUser(book.username, (err, user) => {
             if (err) {cb(err);}
             else {
-                cb(null, data);
+                var obj = {title: book.book, owner: user._id};
+                Book.create(obj, (err, bookData) => {
+                    if (err) {cb(err);}
+                    else {
+                        user.addBook(bookData._id, err => {
+                            if (err) {cb(err);}
+                            else {
+                                cb(null, bookData);
+                            }
+
+                        });
+
+                    }
+                });
+
             }
+
         });
+    }
+
+    /* Deletes one book from the book collection and removes it also from the
+     * user.*/
+    deleteBook(book, cb) {
+        User.getUser(book.username, (err, user) => {
+            if (err) {cb(err);}
+        });
+
     }
 
 }
