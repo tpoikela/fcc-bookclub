af8422ae5c7d807a80eb375eb97dae371b0ac190
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-05 15:15:17 +0100

    Added BooksTop.

--- client/jsx/app.jsx
+++ client/jsx/app.jsx
@@ -3,9 +3,11 @@
 
 const BookWyrmsTop = require('./top.jsx');
 const ProfileTop = require('./profile.jsx');
+const BooksTop = require('./books-top.jsx');
 
 var mainApp = document.querySelector('#main-app');
 var profileTop = document.querySelector('#profile-app');
+var booksTop = document.querySelector('#books-app');
 
 const React = require('react');
 const ReactDOM = require('react-dom');
@@ -15,5 +17,8 @@ if (mainApp) {
 }
 else if (profileTop) {
     ReactDOM.render(<ProfileTop />, profileTop);
+}
+else if (booksTop) {
+    ReactDOM.render(<BooksTop />, booksTop);
 }
 
