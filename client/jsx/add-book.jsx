5ef0ac225d2f1f1bb666d5abf1e4886261beb1bc
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-12 15:28:09 +0100

    Added onClickSearch. Shows now the results as a list.

--- client/jsx/add-book.jsx
+++ client/jsx/add-book.jsx
@@ -10,6 +10,7 @@ class AddBook extends React.Component {
 
         this.onChange = this.onChange.bind(this);
         this.onClickAdd = this.onClickAdd.bind(this);
+        this.onClickSearch = this.onClickSearch.bind(this);
 		this.state = {
 			bookName: ''
 		};
@@ -20,11 +21,45 @@ class AddBook extends React.Component {
 		this.setState({bookName: bookName});
     }
 
-	onClickAdd() {
-		this.props.onClickAdd(this.state.bookName);
+	onClickAdd(book) {
+		this.props.onClickAdd(book.volumeInfo.title);
+	}
+
+	onClickSearch() {
+		this.props.onClickSearch(this.state.bookName);
 	}
 
     render() {
+
+        // Generate the search results here
+        var searchResults = this.props.searchResults.map( (item, index) => {
+            var addCallback = this.onClickAdd.bind(this, item);
+            var volInfo = item.volumeInfo;
+            console.log('volInfo: ' + JSON.stringify(volInfo));
+
+            var thumbnail = null;
+            if (volInfo.imageLinks) {
+                if (volInfo.imageLinks.smallThumbnail) {
+                    thumbnail = <img src={volInfo.imageLinks.smallThumbnail}/>;
+                }
+            }
+            else {
+                console.log('No imageLinks for volInfo');
+
+            }
+
+            return (
+                <div className='book-item' key={index} >
+                    {thumbnail}
+                    Title: {volInfo.title}
+                    Year: {volInfo.publishedData}
+                    Pages: {volInfo.pageCount}
+                    <button onClick={addCallback}>Add to Profile</button>
+                </div>
+
+            );
+        });
+
         return (
             <div className='add-book'>
 				<input className='fa fa-2x search-input'
@@ -33,10 +68,10 @@ class AddBook extends React.Component {
                     placeholder='What books have you?'
                     value={this.state.bookName}
 				/>
-                <button id='add-button' onClick={this.onClickAdd}>
+                <button id='add-button' onClick={this.onClickSearch}>
                     <i className='search-icon fa fa-search fa-2x'/>
                 </button>
-
+                {searchResults}
             </div>
 
         );
@@ -45,7 +80,9 @@ class AddBook extends React.Component {
 }
 
 AddBook.propTypes = {
-	onClickAdd: React.PropTypes.func
+	onClickAdd: React.PropTypes.func,
+    onClickSearch: React.PropTypes.func,
+    searchResults: React.PropTypes.array
 };
 
 module.exports = AddBook;
