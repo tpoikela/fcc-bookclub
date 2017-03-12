030c13c02bdea9a9f78d68e83f9cb407309035e2
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-12 15:29:42 +0100

    Added error checking, and now throws error without proper API key being given.

--- server/common/book-search.js
+++ server/common/book-search.js
@@ -1,15 +1,26 @@
 'use strict';
 
-// https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
-//
+const debug = require('debug')('book:book-search');
+
+// Example of the Google books API call:
+// https://www.googleapis.com/books/v1/volumes?
+// q=flowers+inauthor:keyes&key=yourAPIKey
 
 const request = require('request');
 
 class BookSearch {
 
     constructor(obj) {
-        this.apiKey = obj.apiKey;
-        this.baseUrl = "https://www.googleapis.com/books/v1/volumes";
+        if (typeof obj === 'object') {
+            this.apiKey = obj.apiKey;
+            this.baseUrl = 'https://www.googleapis.com/books/v1/volumes';
+            if (!this.apiKey) {
+                throw new Error('apiKey must be given!');
+            }
+        }
+        else {
+            throw new Error('Constr takes only object');
+        }
     }
 
     search(obj, cb) {
@@ -17,14 +28,25 @@ class BookSearch {
         var key = 'key=' + this.apiKey;
         var url = this.baseUrl + query + '&' + key;
         request.get(url, (error, resp, body) => {
-            console.log('error: ' + error);
-            console.log('resp ' + JSON.stringify(resp));
-            console.log('body: ' + body);
-            cb(error, resp, body);
+            if (!error) {
+                debug('Book search with |' + url + '| completed OK');
+            }
+            else {
+                debug('Book search error: ' + error);
+            }
+            if (body) {
+                try {
+                    var bodyParsed = JSON.parse(body);
+                    cb(null, bodyParsed);
+                }
+                catch (e) {
+                    cb(e);
+                }
+            }
         });
     }
 
-};
+}
 
 
 module.exports = BookSearch;
