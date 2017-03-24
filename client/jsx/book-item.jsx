0453cf111669ae81f5a190402718862829824c41
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-25 00:32:13 +0100

    Added hideInfo prop.

--- client/jsx/book-item.jsx
+++ client/jsx/book-item.jsx
@@ -16,7 +16,7 @@ class BookItem extends React.Component {
         if (volInfo.imageLinks) {
             if (volInfo.imageLinks.smallThumbnail) {
                 thumbnail = (
-                    <img className='book-item-thumb'
+                    <img className='img-responsive book-item-thumb'
                     src={volInfo.imageLinks.smallThumbnail}
                     />);
             }
@@ -30,19 +30,27 @@ class BookItem extends React.Component {
             authorText = volInfo.authors[0];
         }
 
+        var info = (
+            <li className='book-item-li'>
+                <ul>
+                    <li>Title: {volInfo.title}</li>
+                    <li>Authors: {authorText}</li>
+                    <li>Published on: {volInfo.publishedDate}</li>
+                    <li>Pages: {volInfo.pageCount}</li>
+                </ul>
+            </li>
+        );
+
+        if (this.props.hideInfo) {
+            info = null;
+        }
+
         return (
             <div className={className}>
                 <ul className='book-item-ul'>
                     <li className='book-item-li'>{thumbnail}</li>
-                    <li className='book-item-li'>
-                        <ul>
-                            <li>Title: {volInfo.title}</li>
-                            <li>Authors: {authorText}</li>
-                            <li>Published on: {volInfo.publishedDate}</li>
-                            <li>Pages: {volInfo.pageCount}</li>
-                        </ul>
-                    </li>
                 </ul>
+                {info}
             </div>
         );
 
@@ -51,7 +59,8 @@ class BookItem extends React.Component {
 
 BookItem.propTypes = {
     book: React.PropTypes.object,
-    className: React.PropTypes.string
+    className: React.PropTypes.string,
+    hideInfo: React.PropTypes.bool
 };
 
 module.exports = BookItem;
