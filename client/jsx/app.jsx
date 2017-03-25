40d591a3f51af0ef4f9af1ef3b8c48d6d9c5e9dc
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-03-25 22:14:00 +0100

    Removed useless top.jsx. Added meta tags for viewport to make the responsive design work.

--- client/jsx/app.jsx
+++ client/jsx/app.jsx
@@ -1,21 +1,16 @@
 
 'use strict';
 
-const BookWyrmsTop = require('./top.jsx');
 const ProfileTop = require('./profile.jsx');
 const BooksTop = require('./books-top.jsx');
 
-var mainApp = document.querySelector('#main-app');
 var profileTop = document.querySelector('#profile-app');
 var booksTop = document.querySelector('#books-app');
 
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-if (mainApp) {
-    ReactDOM.render(<BookWyrmsTop />, mainApp);
-}
-else if (profileTop) {
+if (profileTop) {
     ReactDOM.render(<ProfileTop />, profileTop);
 }
 else if (booksTop) {
